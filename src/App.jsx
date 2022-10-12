ee747f9a22bfc62096d003bcb57fe70659e679c2
Joseph Urso <93886898+JoeUrso@users.noreply.github.com>
2022-10-12 14:15:18 -0700

    Import componets into app,jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,36 @@
 import React from "react";
+import {
+    Billing,
+    Business,
+    CardDeal,
+    Clients,
+    CTA,
+    Footer,
+    Hero,
+    Navbar,
+    Stats,
+    Testimonials,
+} from "./components";
 import styles from "./styles";
 
 const App = () => (
     <div className="w-full overflow-hidden bg-primary">
         <div className={`${styles.paddingX} ${styles.flexCenter}`}>
-            <div className={`${styles.boxWidth}`}>Navbar</div>
+            <div className={`${styles.boxWidth}`}>
+                <Navbar />
+            </div>
         </div>
 
         <div className={`${styles.flexStart} bg-primary`}>
-            <div className={`${styles.boxWidth}`}>Hero</div>
+            <div className={`${styles.boxWidth}`}>
+                <Hero />
+            </div>
         </div>
 
         <div className={`${styles.flexStart} ${styles.paddingX} bg-primary`}>
             <div className={`${styles.boxWidth}`}>
-                Stats Business Billing CardDeal Testimonials Clients CTA Footer
+                <Stats /> <Business /> <Billing /> <CardDeal /> <Testimonials />
+                <Clients /> <CTA /> <Footer />
             </div>
         </div>
     </div>
